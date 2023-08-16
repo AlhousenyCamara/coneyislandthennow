2a6a1d47d93cf8e45cbfc207fb44f5d2eff2b81b
Alhouseny Camara <camaraalhouseny44@gmail.com>
2023-08-16 13:58:52 -0400

    flyer added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,18 +15,22 @@ export default function Home() {
           <div className="absolute inset-0 bg-black opacity-80"></div>
           <div className="absolute inset-0 bg-cover bg-center" style={{backgroundImage: 'url(new.png)'}}></div>
         </div>
-        <div className="absolute inset-0 flex items-center justify-center">
-          <h1 className="text-4xl md:text-7xl font-bold text-orange">CONEY ISLAND THEN AND NOW</h1>
+        <div className="absolute inset-0 flex items-center justify-center text-center">
+          <h1 className="text-4xl px-5 md:text-7xl font-bold text-orange">CONEY ISLAND THEN AND NOW</h1>
         </div>
       </div>
-      <section className='md:p-20'>
-    <div className='text-2xl md:text-6xl text-center'>“Coney Island Then and Now” </div>
+      <section className='p-4 md:p-20'>
+    <div className='mt-8 text-2xl md:text-6xl text-center'>“Coney Island Then and Now” </div>
     <div className='text-lg md:text-2xl text-center mt-5 md:mt-8'> Tells the story of one of the most magical places in the world. Not exactly Disney World.. This gritty documentary film shows the evolution of Coney Island, Brooklyn’s famous beachfront Amusement Park which has made many transformations over the years. From visionary George Tilyou’s Steeplechase amusement park  to land baron Fred Trump’s controversial Trump Village, and now rapper and Coney Island native Gorilla Nems is changing things with the times. </div>
     <div className='text-lg md:text-2xl text-center mt-5'> There are many stories in this very mixed community made up of Russians, Latinos, Asians, and African Americans creating an interesting dichotomy where old Coney Island and new collide with gentrification and a booming real estate market. Even if you have never been to Coney Island its iconic symbols like the Wonder Wheel, The Cyclone, Nathan’s Hot Dogs, The Mermaid Parade, Graffiti Walk, and Coney Island USA Sideshow will be. This is a special place and a story that must be told.</div>
     <div className='text-2xl md:text-5xl text-center mt-5 mb-8 text-orange'>3 Avenue 21 blocks infinite stories.</div>
     <div className='text-2xl md:text-4xl text-center'>“Coney Island Then and Now” </div>
-    <div className='flex flex-row justify-between text-2xl md:text-2xl text-center mt-5 mb-8 text-white'>
-      <span>Produced By: KIMBERLY GRAY </span>
+      <div className="flex justify-center mt-5">
+    <img src="post1.jpeg" alt="Flyer 1" className="w-1/3 mx-2"/>
+    <img src="post2.png" alt="Flyer 2" className="w-1/3 mx-2"/>
+  </div>
+    <div className='flex flex-row justify-center text-2xl md:text-2xl text-center mt-5 mb-8 text-white'>
+      <span className='mr-8'>Produced By: KIMBERLY GRAY </span>
       <span> Directed By: MARIA CLARA</span>
     </div>
     <div className='text-2xl md:text-4xl text-center'>Starring: GORILLA NEMS  DANIELLE MASTRION XAVIER FRANKS DICK ZIGUN WENDY BLADESBRENDAN SCHWEDA ERIC KNAPP AKA &quot;MR.CYCLONE&quot; ALAN ZINDMAN
@@ -36,7 +40,7 @@ export default function Home() {
     </div>
     <div className='text-lg md:text-2xl text-center mt-5'>“Coney Island: Then and Now” is a documentary film about the evolution of Coney Island, Brooklyn’s famous beachfront Amusement Park which has made many transformations over the years. The film details the history and nostalgia of Coney Island’s boardwalk, including the iconic landmarks the Wonder Wheel, Cyclone, Nathan’s Hot Dogs, The Mermaid Parade, Graffiti Walk, and Coney Island USA Sideshow.</div>
     <div className='text-lg text-center mt-5'>IG @coneyislandthenandnow
-    Copyright 2023 (©)</div>
+    Copyright 2023(©)</div>
   </section>
 </main>
   )
